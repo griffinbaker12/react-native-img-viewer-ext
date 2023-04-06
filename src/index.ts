aab3f78d05a4e2a746603884493baf53925398dc
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2023-04-06 17:24:31 -0500

    tweaks

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ import ImageViewer from "./image-viewer/image-viewer.component"
 import { Props as ImageViewerPropsDefine } from "./image-viewer/image-viewer.type";
 
 export { ImageViewer, ImageViewerPropsDefine }
-
